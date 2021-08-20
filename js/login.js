06ecee2b01b41b5a28659e5403d86a01a992995d
manuelparedess <luisparedescuenca@gmail.com>
2021-08-20 17:50:03 -0300

    Entrega 1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+    document.getElementById("login-submit").addEventListener("click", function (e) {
 
+        let email = document.getElementById("login-email")
+        let password = document.getElementById("login-password")
+        let alerta = document.getElementById("alerta")
+        let camposCompletados = true
+
+        if (email.value === "") {
+            camposCompletados = false
+        }
+
+        if (password.value === "") {
+            camposCompletados = false
+        }
+
+        if (camposCompletados) {
+            window.location = "inicio.html"
+        } else {
+            alerta.classList.add("invalido")
+            alerta.innerHTML = `* Debe completar todos los campos para iniciar sesion`
+        }
+
+    })
 });
--- js/products.js
+++ js/products.js
@@ -1,6 +1,49 @@
+let productos=[]
+
+function cargarProductos(array) {
+
+    let listado = ""
+
+    for (let i = 0; i < array.length; i++) {
+        const producto = array[i];
+
+        listado += `
+        <div class="product">
+          <div class="products-image">
+            <img id="product-image" src="`+ producto.imgSrc +`">
+          </div>
+          <div class="products-name"> 
+            <h3 id="product-name">`+ producto.name +`</h3>
+          </div>
+          <div class="products-description">
+            <p id="product-description" style="font-style: oblique;">`+ producto.description +`</p>
+          </div>
+          <div class="products-cost">
+            <h4 id="product-cost"> $`+ producto.cost +` `+ producto.currency +`</h4>
+          </div>
+          <div class="products-soldcount">
+            <p id="soldcount">Vendidos: ` + producto.soldCount +`</p>
+          </div>
+        </div>
+        <br><br>
+        `
+        
+        document.getElementById("products-list").innerHTML = listado
+    }
+}
+
+
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function (e) {
-
-});
+    getJSONData(PRODUCTS_URL).then(function(respuesta){
+        if (respuesta.status === "ok"){
+            productos = respuesta.data;
+            cargarProductos(productos);
+        } else {
+            alert(respuesta.data)
+        }
+    });
+})
